e0360349ef8742a10e8f933c65638b1ce4e9fd74
Auca Maillot <aucacoyan@gmail.com>
2022-06-04 07:35:43 -0300

    add details on Copy-Item

--- clone-pwsh/test.ps1
+++ clone-pwsh/test.ps1
@@ -10,3 +10,12 @@ New-Item -Path . -Name "newfolder" -ItemType "directory"
 #  copiar en esa carpeta el bot de rocketbot
 #  copiar los archivos __init__.py y package.json en /carpeta/modules/<nombre de la carpeta>/
 #  correr rocketbot?
+
+# Para copiar más de un archivo necesitas
+# -Path <archivos separados por coma> 
+# ex: -Path ./file1,./folder1
+# -Destination <dest>
+# -Recurse si queres copiar todos los archivos 
+# -Force si tenés que reemplazar archivos
+Copy-Item -PATH .\.github\ -Destination C:\Users\aucac\Code\exp-flutter\ -Recurse -Force
+
